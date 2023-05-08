800127ea199ac3915f24e111f7f8e6eacecffa0b
pangzhenzhou <hljpzz1982@163.com>
2023-05-08 18:36:32 +0800

    MonographDB RESTful API support start/stop monitor (#25)
    * fixed monograph status command
    
    * rest api supprt start_monitor cmd
    
    * rest api supprt stop_monitor cmd

--- src/rest_api/src/handler.rs
+++ src/rest_api/src/handler.rs
@@ -9,6 +9,18 @@ use serde_json::{json, Value};
 use std::collections::HashMap;
 // use tracing::info;
 
+const SUPPORT_CTL_CMD: &[&str; 5] = &["start", "stop", "install", "start_monitor", "stop_monitor"];
+const SUPPORT_CTL_STATUS_CMD: &[&str; 8] = &[
+    "start",
+    "stop",
+    "install",
+    "status",
+    "deploy",
+    "run-deps",
+    "start_monitor",
+    "stop_monitor",
+];
+
 #[get("/check_health")]
 pub async fn check_health() -> impl Responder {
     HttpResponse::Ok().content_type("text/plain").body("ok")
@@ -52,6 +64,14 @@ fn build_command_from_str(cmd_str: &str, cluster: Option<String>) -> CommandArgs
         "run-deps" => CommandArgs::RunDeps {
             topology_file: "_NONE".to_string(),
         },
+        "start_monitor" => CommandArgs::Monitor {
+            cluster: cluster.unwrap(),
+            command: "start".to_string(),
+        },
+        "stop_monitor" => CommandArgs::Monitor {
+            cluster: cluster.unwrap(),
+            command: "stop".to_string(),
+        },
         _ => unreachable!(),
     }
 }
@@ -92,7 +112,7 @@ pub async fn ctl_cluster(
     param: web::Path<(String, String)>,
 ) -> Result<impl Responder, WebHandleError> {
     let (cluster, command) = param.into_inner();
-    validate_command(command.as_str(), &["start", "stop", "install"])?;
+    validate_command(command.as_str(), SUPPORT_CTL_CMD)?;
     let ctl_command = build_command_from_str(command.as_str(), Some(cluster));
     global_handler.submit(RequestPayload {
         command: Some(ctl_command),
@@ -107,10 +127,7 @@ pub async fn check_cmd_status(
     param: web::Path<(String, String)>,
 ) -> Result<impl Responder, WebHandleError> {
     let (cluster, command) = param.into_inner();
-    validate_command(
-        command.as_str(),
-        &["start", "stop", "install", "status", "deploy", "run-deps"],
-    )?;
+    validate_command(command.as_str(), SUPPORT_CTL_STATUS_CMD)?;
 
     let cmd_executor = global_handler.get_command_executor();
     let deployment_config_opt = cmd_executor
@@ -138,6 +155,7 @@ pub async fn check_cmd_status(
             .await?;
         let mut success = Vec::new();
         let mut failure = Vec::new();
+
         completed_task_vec.iter().for_each(|task_status| {
             let task_id = TaskId::from_json_string(task_status.clone().task);
             let update_timestamp = task_status
@@ -156,10 +174,6 @@ pub async fn check_cmd_status(
                 failure.push(task_id_with_time);
             }
         });
-
-        let tmp_task_id_len = task_ids.len();
-        let complete_task_len = completed_task_vec.len();
-        println!("#### complete_task_len={complete_task_len:?},task_id.len={tmp_task_id_len:?}");
         // info!("/status/cluster/command all_task_id = {:#?}", task_ids);
         let status = if !failure.is_empty() {
             "failure"
