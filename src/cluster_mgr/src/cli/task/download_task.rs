2cdc45dc2757ade74a8a4706d4ab1c5a26006912
zhanghao <zhanghao997@qq.com>
2024-04-26 10:37:02 +0800

    update download url

--- src/cluster_mgr/src/cli/task/download_task.rs
+++ src/cluster_mgr/src/cli/task/download_task.rs
@@ -13,10 +13,11 @@ use indicatif::{MultiProgress, ProgressBar};
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::ffi::{OsStr, OsString};
+use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
 use std::time::Duration;
-use tracing::{error, info};
+use tracing::{error, info, warn};
 
 pub(crate) const DOWNLOAD_URL: &str = "download_url";
 pub(crate) const DOWNLOAD_FILE_NAME: &str = "download_file_name";
@@ -26,6 +27,7 @@ pub(crate) const DOWNLOAD_PATH: &str = "download_path";
 pub struct DownloadTask {
     task_id: TaskId,
     pg_bar: ProgressBar,
+    client: reqwest::Client,
 }
 
 impl DownloadTask {
@@ -70,6 +72,9 @@ impl DownloadTask {
             download_url_vec.push(Codis::download_url());
         }
 
+        let client = reqwest::Client::builder()
+            .connect_timeout(Duration::from_secs(5))
+            .build()?;
         let mpg_bar = MultiProgress::new();
         let local_ip = local_ip_address::local_ip()?.to_string();
         let download_tasks = download_url_vec
@@ -95,7 +100,7 @@ impl DownloadTask {
                             (DOWNLOAD_FILE_NAME.to_string(), TaskArgValue::Str(filename)),
                             (DOWNLOAD_PATH.to_string(), TaskArgValue::Str(download_dir)),
                         ]),
-                        task: Box::new(DownloadTask::new(task_id, pb)),
+                        task: Box::new(DownloadTask::new(task_id, pb, client.clone())),
                         task_host: TaskHost::Local,
                     },
                 )
@@ -104,8 +109,12 @@ impl DownloadTask {
         Ok(download_tasks)
     }
 
-    pub fn new(task_id: TaskId, pg_bar: ProgressBar) -> Self {
-        Self { task_id, pg_bar }
+    pub fn new(task_id: TaskId, pg_bar: ProgressBar, client: reqwest::Client) -> Self {
+        Self {
+            task_id,
+            pg_bar,
+            client,
+        }
     }
 }
 
@@ -120,94 +129,66 @@ impl TaskExecutor for DownloadTask {
         _task_host: TaskHost,
         task_input: HashMap<String, TaskArgValue>,
     ) -> anyhow::Result<Option<ExecutionValue>> {
-        println!("{} execute.\n", self.task_id.pretty_string());
-        let download_url =
+        info!("{} execute.\n", self.task_id.pretty_string());
+        let url =
             TaskArgValue::into_inner_value::<String>(task_input.get(DOWNLOAD_URL).unwrap().clone());
         let filename = TaskArgValue::into_inner_value::<String>(
             task_input.get(DOWNLOAD_FILE_NAME).unwrap().clone(),
         );
-        let download_path = PathBuf::from(TaskArgValue::into_inner_value::<String>(
+        let save_dir = PathBuf::from(TaskArgValue::into_inner_value::<String>(
             task_input.get(DOWNLOAD_PATH).unwrap().clone(),
         ));
 
-        // create local directory and partial file
-        let create_download_path_rs = std::fs::create_dir_all(download_path.as_path());
-        if create_download_path_rs.is_err() {
-            error!("Download cli create tmp_dir error {:?}", download_path);
-            return Err(anyhow!(DownloadErr(
-                download_url,
-                create_download_path_rs.err().unwrap().to_string()
-            )));
+        let response = self
+            .client
+            .get(&url)
+            .send()
+            .await
+            .map_err(|err| DownloadErr(url.clone(), err.to_string()))?;
+        let status = response.status();
+        if !status.is_success() {
+            error!("Download falied http status_code = {:?}", status.as_str());
+            return Err(anyhow!(DownloadErr(url, status.to_string())));
         }
-        let local_file_path = download_path.join(&filename);
-        if local_file_path.exists() {
-            info!(
-                "The local file {:?} exists. please delete it if you want to re-download it first.",
-                local_file_path.clone()
-            );
-            return Ok(None);
+        let file_len = response.content_length().unwrap();
+
+        // create local directory and partial file
+        if let Err(err) = std::fs::create_dir_all(save_dir.as_path()) {
+            error!("Download task: create directory {:?} falied", save_dir);
+            return Err(anyhow!(DownloadErr(url, err.to_string())));
+        }
+        let save_path = save_dir.join(&filename);
+        if save_path.exists() {
+            // TODO(zhanghao): It would be better to check file SHA
+            if file_len == fs::metadata(&save_path).unwrap().len() {
+                warn!("local file cache {:?} found.", save_path);
+                return Ok(None);
+            }
         }
         // TODO(zhanghao): Use HTTP range header to resume download
-        let tmp_file = append_ext(local_file_path.clone(), "partial");
-        let create_local_file_rs = std::fs::File::create(tmp_file.as_path());
-        if create_local_file_rs.is_err() {
-            return Err(anyhow!(DownloadErr(
-                download_url,
-                create_local_file_rs.err().unwrap().to_string()
-            )));
-        }
-        let mut download_file = create_local_file_rs.unwrap();
+        let part_path = append_ext(save_path.clone(), "partial");
+        let mut part_file = std::fs::File::create(part_path.as_path())
+            .map_err(|err| DownloadErr(url.clone(), err.to_string()))?;
 
-        // start download
-        let client = reqwest::Client::builder()
-            .connect_timeout(Duration::from_secs(5))
-            .build()?;
-        let rsp_rs = client.get(download_url.as_str()).send().await;
-        if let Err(rsp_err) = rsp_rs {
-            error!(
-                "Download cli error cause by http client request error {:?}",
-                rsp_err
-            );
-            return Err(anyhow!(DownloadErr(
-                download_url.clone(),
-                rsp_err.to_string()
-            )));
-        }
-        let http_response = rsp_rs.unwrap();
-        let status = http_response.status();
-        if !status.is_success() {
-            error!(
-                "Download cli error cause by http status_code = {:?}",
-                status.as_str()
-            );
-            return Err(anyhow!(DownloadErr(download_url, status.to_string())));
-        }
-        let file_len = http_response.content_length().unwrap();
+        // start downloading
         self.pg_bar.set_length(file_len);
-        let mut stream_reader = http_response.bytes_stream();
+        let mut stream_reader = response.bytes_stream();
         while let Some(stream_chunk) = stream_reader.next().await {
             if let Err(err) = stream_chunk {
-                error!(
-                    "DownloadRemote task error file={},msg={}",
-                    download_url, err
-                );
-                return Err(anyhow!(DownloadErr(download_url, err.to_string())));
+                error!("DownloadRemote task error file={},msg={}", url, err);
+                return Err(anyhow!(DownloadErr(url, err.to_string())));
             }
             let chunk = stream_chunk.unwrap();
-            if let Err(write_err) = download_file.write_all(&chunk) {
-                error!(
-                    "DownloadTask {} write local file error {} ",
-                    download_url, write_err
-                );
-                return Err(anyhow!(DownloadErr(download_url, write_err.to_string())));
+            if let Err(write_err) = part_file.write_all(&chunk) {
+                error!("DownloadTask {} write local file error {} ", url, write_err);
+                return Err(anyhow!(DownloadErr(url, write_err.to_string())));
             }
             self.pg_bar.inc(chunk.len() as u64);
         }
-        if let Err(err) = std::fs::rename(tmp_file, local_file_path.as_path()) {
-            return Err(anyhow!(DownloadErr(download_url, err.to_string())));
-        }
         self.pg_bar
             .finish_with_message(format!("{filename} download compete"));
+        fs::rename(part_path, save_path.as_path())
+            .map_err(|err| DownloadErr(url, err.to_string()))?;
 
         let mut download_result = HashMap::new();
         download_result.insert(
@@ -216,7 +197,7 @@ impl TaskExecutor for DownloadTask {
         );
         download_result.insert(
             CMD_OUTPUT.to_string(),
-            TaskArgValue::Str(local_file_path.to_str().unwrap().to_string()),
+            TaskArgValue::Str(save_path.to_str().unwrap().to_string()),
         );
         download_result.insert(CMD_STATUS.to_string(), TaskArgValue::Number(0));
         // Ok(Some(download_result))
