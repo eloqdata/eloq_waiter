1337f96c26ac64bfd43e1b726eb4ca2150859f7d
Hao Zhang <zhanghao997@qq.com>
2024-04-29 14:38:02 +0800

    1. better demo command & 2. add concourse CI (#112)
    1. better demo command 
    2. add concourse CI

--- src/cluster_mgr/src/cli/task/check_task.rs
+++ src/cluster_mgr/src/cli/task/check_task.rs
@@ -5,9 +5,8 @@ use crate::{
     },
     config::{config_base::DeploymentConfig, DeploymentPackage},
 };
-use anyhow::Ok;
+use anyhow::{bail, Ok, Result};
 use std::collections::HashMap;
-use tracing::error;
 
 #[derive(Debug, Clone)]
 pub struct CheckTask {
@@ -21,26 +20,26 @@ impl CheckTask {
         CheckTask { kind, host, config }
     }
 
-    async fn check_tx_sv(&self, host: TaskHost) -> anyhow::Result<Option<ExecutionValue>> {
+    async fn check_tx_sv(&self, host: TaskHost) -> Result<Option<ExecutionValue>> {
         let ssh_k = self.config.connection.ssh_auth_key().unwrap();
         let sess = ssh::SSHSession::from_task_host(host, ssh_k).await?;
         for p in sess.used_tcp_ports().await? {
             if self.config.deployment.port.contains(p) {
-                error!("tx-service socket {}:{p} is already used", self.host);
+                bail!("tx-service socket {}:{p} is already used", self.host);
             }
             if let Some(moni) = &self.config.deployment.monitor {
                 if moni.node_exporter_port == p {
-                    error!("node exporter socket {}:{p} is already used", self.host);
+                    bail!("node exporter socket {}:{p} is already used", self.host);
                 }
                 if moni.mysql_exporter_port == p {
-                    error!("mysql exporter socket {}:{p} is already used", self.host);
+                    bail!("mysql exporter socket {}:{p} is already used", self.host);
                 }
             }
         }
         Ok(None)
     }
 
-    async fn check_log_sv(&self, host: TaskHost) -> anyhow::Result<Option<ExecutionValue>> {
+    async fn check_log_sv(&self, host: TaskHost) -> Result<Option<ExecutionValue>> {
         let needed = self
             .config
             .deployment
@@ -52,11 +51,11 @@ impl CheckTask {
         let sess = ssh::SSHSession::from_task_host(host, ssh_k).await?;
         for p in sess.used_tcp_ports().await? {
             if needed.contains(&p) {
-                error!("log-service socket {}:{p} is already used", self.host);
+                bail!("log-service socket {}:{p} is already used", self.host);
             }
             if let Some(moni) = &self.config.deployment.monitor {
                 if moni.node_exporter_port == p {
-                    error!("node exporter socket {}:{p} is already used", self.host);
+                    bail!("node exporter socket {}:{p} is already used", self.host);
                 }
             }
         }
@@ -67,22 +66,22 @@ impl CheckTask {
         &self,
         host: TaskHost,
         input: HashMap<String, TaskArgValue>,
-    ) -> anyhow::Result<Option<ExecutionValue>> {
+    ) -> Result<Option<ExecutionValue>> {
         assert!(self.config.deployment.storage_service.cassandra.is_some());
         let ssh_k = self.config.connection.ssh_auth_key().unwrap();
         let sess = ssh::SSHSession::from_task_host(host, ssh_k).await?;
         for p in sess.used_tcp_ports().await? {
             if let Some(v) = input.get(&p.to_string()) {
                 let name = v.clone().into_inner_value::<String>();
-                error!("cassandra {name} socket {}:{p} is already used", self.host);
+                bail!("cassandra {name} socket {}:{p} is already used", self.host);
             }
             if let Some(moni) = &self.config.deployment.monitor {
                 if moni.node_exporter_port == p {
-                    error!("node exporter socket {}:{p} is already used", self.host);
+                    bail!("node exporter socket {}:{p} is already used", self.host);
                 }
                 if let Some(cc) = &moni.cassandra_collector {
                     if cc.mcac_port == p {
-                        error!("cassandra mcac socket {}:{p} is already used", self.host);
+                        bail!("cassandra mcac socket {}:{p} is already used", self.host);
                     }
                 }
             }
@@ -90,7 +89,7 @@ impl CheckTask {
         Ok(None)
     }
 
-    async fn check_prometheus(&self, host: TaskHost) -> anyhow::Result<Option<ExecutionValue>> {
+    async fn check_prometheus(&self, host: TaskHost) -> Result<Option<ExecutionValue>> {
         let ssh_k = self.config.connection.ssh_auth_key().unwrap();
         let sess = ssh::SSHSession::from_task_host(host, ssh_k).await?;
         let port = self
@@ -103,13 +102,13 @@ impl CheckTask {
             .port;
         for p in sess.used_tcp_ports().await? {
             if port == p {
-                error!("prometheus socket {}:{p} is already used", self.host);
+                bail!("prometheus socket {}:{p} is already used", self.host);
             }
         }
         Ok(None)
     }
 
-    async fn check_grafana(&self, host: TaskHost) -> anyhow::Result<Option<ExecutionValue>> {
+    async fn check_grafana(&self, host: TaskHost) -> Result<Option<ExecutionValue>> {
         let ssh_k = self.config.connection.ssh_auth_key().unwrap();
         let sess = ssh::SSHSession::from_task_host(host, ssh_k).await?;
         let port = self
@@ -122,13 +121,13 @@ impl CheckTask {
             .port;
         for p in sess.used_tcp_ports().await? {
             if port == p {
-                error!("grafana socket {}:{p} is already used", self.host);
+                bail!("grafana socket {}:{p} is already used", self.host);
             }
         }
         Ok(None)
     }
 
-    async fn check_codis(&self, host: TaskHost) -> anyhow::Result<Option<ExecutionValue>> {
+    async fn check_codis(&self, host: TaskHost) -> Result<Option<ExecutionValue>> {
         let codis = self.config.deployment.codis.as_ref().unwrap();
         let need = if self.host == codis.dashboard {
             vec![18080]
@@ -141,11 +140,11 @@ impl CheckTask {
         let sess = ssh::SSHSession::from_task_host(host, ssh_k).await?;
         for p in sess.used_tcp_ports().await? {
             if need.contains(&p) {
-                error!("codis socket {}:{p} is already used", self.host);
+                bail!("codis socket {}:{p} is already used", self.host);
             }
             if let Some(moni) = &self.config.deployment.monitor {
                 if moni.node_exporter_port == p {
-                    error!("node exporter socket {}:{p} is already used", self.host);
+                    bail!("node exporter socket {}:{p} is already used", self.host);
                 }
             }
         }
@@ -167,7 +166,7 @@ impl TaskExecutor for CheckTask {
         &self,
         host: TaskHost,
         input: HashMap<String, TaskArgValue>,
-    ) -> anyhow::Result<Option<ExecutionValue>> {
+    ) -> Result<Option<ExecutionValue>> {
         match self.kind {
             DeploymentPackage::MonographTx => self.check_tx_sv(host).await,
             DeploymentPackage::Storage => self.check_kv_store(host, input).await,
