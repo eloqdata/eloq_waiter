1771fc7857ba172ac748bff9f0a91d5ab62963d4
zhanghao <zhanghao997@qq.com>
2024-06-01 17:37:47 +0800

    fix tests

--- tests/basic.sh
+++ tests/basic.sh
@@ -5,7 +5,7 @@ echo ">>> Test Demo command"
 
 # test eloq-sql
 cluster_mgr demo eloq-sql --version nightly
-CLIENT=$(cluster_mgr connect demo-sql-cassandra)
+CLIENT=$(cluster_mgr -q connect demo-sql-cassandra)
 cluster_mgr status demo-sql-cassandra --wait 5
 eval "${CLIENT} --execute 'SHOW DATABASES'"
 cluster_mgr monitor demo-sql-cassandra stop
@@ -19,7 +19,7 @@ cluster_mgr stop demo-sql-cassandra --all
 # test eloq-kv
 sleep 15
 cluster_mgr demo eloq-kv --version nightly
-CLIENT=$(cluster_mgr connect demo-kv-cassandra)
+CLIENT=$(cluster_mgr -q connect demo-kv-cassandra)
 cluster_mgr status demo-kv-cassandra --wait 5
 eval ${CLIENT} incr mycounter
 eval ${CLIENT} get mycounter
@@ -34,7 +34,7 @@ cluster_mgr remove demo-kv-cassandra
 
 sleep 15
 cluster_mgr demo eloq-kv --store rocks
-CLIENT=$(cluster_mgr connect demo-kv-rocksdb)
+CLIENT=$(cluster_mgr -q connect demo-kv-rocksdb)
 cluster_mgr status demo-kv-rocksdb --wait 5
 eval ${CLIENT} incr mycounter
 eval ${CLIENT} get mycounter
@@ -49,7 +49,7 @@ echo ">>> Test Launch command"
 
 sleep 15
 cluster_mgr launch ${CLUSTER_MGR_HOME}/config/examples/eloqsql_cassandra.yaml
-CLIENT=$(cluster_mgr connect eloqsql-cluster)
+CLIENT=$(cluster_mgr -q connect eloqsql-cluster)
 cluster_mgr status eloqsql-cluster --wait 5
 eval "${CLIENT} --execute 'SHOW DATABASES'"
 cluster_mgr monitor eloqsql-cluster stop 
@@ -58,7 +58,7 @@ cluster_mgr inspect eloqsql-cluster
 
 sleep 15
 cluster_mgr launch ${CLUSTER_MGR_HOME}/config/examples/eloqkv_rocksdb.yaml
-CLIENT=$(cluster_mgr connect eloqkv-cluster)
+CLIENT=$(cluster_mgr -q connect eloqkv-cluster)
 cluster_mgr status eloqkv-cluster --wait 5
 eval ${CLIENT} incr mycounter
 eval ${CLIENT} get mycounter
@@ -79,7 +79,7 @@ sed -i "s|127.0.0.1|${MY_IP}|g" ${CLUSTER_MGR_HOME}/config/examples/eloqkv_cassa
 
 sleep 15
 cluster_mgr launch ${CLUSTER_MGR_HOME}/config/examples/eloqsql_cassandra.yaml
-CLIENT=$(cluster_mgr connect eloqsql-cluster)
+CLIENT=$(cluster_mgr -q connect eloqsql-cluster)
 cluster_mgr status eloqsql-cluster --wait 5
 eval "${CLIENT} --execute 'SHOW DATABASES'"
 cluster_mgr monitor eloqsql-cluster stop 
@@ -88,7 +88,7 @@ cluster_mgr inspect eloqsql-cluster
 
 sleep 15
 cluster_mgr launch ${CLUSTER_MGR_HOME}/config/examples/eloqkv_cassandra.yaml
-CLIENT=$(cluster_mgr connect eloqkv-cluster)
+CLIENT=$(cluster_mgr -q connect eloqkv-cluster)
 cluster_mgr status eloqkv-cluster --wait 5
 eval ${CLIENT} incr mycounter
 eval ${CLIENT} get mycounter
