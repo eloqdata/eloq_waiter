6b015c670ee0b5b551bb34a4da2fefcf7f1993e5
zhanghao <zhanghao997@qq.com>
2024-07-08 12:16:27 +0800

    update build-image.bash

--- docker/build-image.bash
+++ docker/build-image.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -exuo
-IMG_KIND=$1
-IMG_OS=$2
 
-rm Dockerfile
-ln -s ${IMG_KIND}-${IMG_OS}.dockerfile Dockerfile
-docker build -t monographdb/waiter-${IMG_KIND}-${IMG_OS} .
-docker push monographdb/waiter-${IMG_KIND}-${IMG_OS}
+build_image() {
+    IMG_KIND=$1
+    IMG_OS=$2
+    rm Dockerfile
+    ln -s ${IMG_KIND}-${IMG_OS}.dockerfile Dockerfile
+    docker build -t monographdb/waiter-${IMG_KIND}-${IMG_OS} .
+    docker push monographdb/waiter-${IMG_KIND}-${IMG_OS}
+}
+
+build_image $1 $2
