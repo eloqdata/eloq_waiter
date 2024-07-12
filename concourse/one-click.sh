64c5b496918afe3f1a43d145ceaebabdc0ef7088
zhanghao <zhanghao997@qq.com>
2024-07-12 12:28:51 +0800

    optional exit on error

--- concourse/one-click.sh
+++ concourse/one-click.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ $ERR_EXIT = true ]; then
+    set -e
+fi
 
 list_jobs() {
     fly -t ${TARGET} jobs -p ${PIPELINE}
@@ -10,7 +13,11 @@ list_jobs | awk '{print $1}' |
     while read JOB; do
         echo "trigger job => ${PIPELINE}/${JOB}"
         time fly -t ${TARGET} trigger-job --job ${PIPELINE}/${JOB} --watch >${PIPELINE}.${JOB} 2>&1
-        echo "finished with $?"
+        case $? in
+        0) echo "Success" ;;
+        3) echo "Aborted" ;;
+        *) echo "Failed ($?)" ;;
+        esac
     done
 
 list_jobs
