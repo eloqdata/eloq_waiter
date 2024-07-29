2b816b62e3cde816d65fde69f5e06641cb8b34a4
Hao Zhang <zhanghao997@qq.com>
2024-07-29 14:52:44 +0800

    fix db controller: start/stop order (#170)

--- src/cluster_mgr/src/cli/task/group/db_cluster_ctrl_group.rs
+++ src/cluster_mgr/src/cli/task/group/db_cluster_ctrl_group.rs
@@ -4,10 +4,10 @@ use crate::cli::task::group::{CtrlDBTaskGroup, TaskGroup};
 use crate::cli::task::monograph_log_ctl_task::MonographLogCtlTask;
 use crate::cli::task::monograph_log_probe_task::MonographLogProbeTask;
 use crate::cli::task::monograph_tx_ctl_task::MonographTxCtlTask;
-use crate::cli::task::task_base::TaskExecutionContext;
+use crate::cli::task::task_base::{TaskExecutionContext, TaskId, TaskInstance};
 use crate::cli::SubCommand;
 use crate::config::config_base::DeployConfig;
-use crate::config::deployment;
+use anyhow::Result;
 use indexmap::IndexMap;
 
 #[async_trait::async_trait]
@@ -16,79 +16,107 @@ impl TaskGroup for CtrlDBTaskGroup {
         &self,
         cmd_arg: SubCommand,
         config: DeployConfig,
-    ) -> anyhow::Result<TaskExecutionContext> {
-        let stop_all = match cmd_arg.clone() {
-            SubCommand::Stop {
-                cluster: _,
-                force: _,
-                all,
-            } => all,
-            _ => false,
-        };
-        let cmd_ref = cmd_arg.as_ref();
-        let is_start_cmd = cmd_ref == "start" || cmd_ref == "restart";
-        let has_cass = config.deployment.storage_service.inner_cass().is_some();
-        // FIXME: stop order
-        let (mut executable, mut barrier) = if has_cass && (is_start_cmd || stop_all) {
-            let exe = CassandraCtlTask::from_config(cmd_arg.clone(), &config);
-            let ba = CassandraCtlTask::start_barrier(exe.len());
-            (exe, ba)
-        } else {
-            (IndexMap::new(), vec![])
-        };
-
-        let batch_cmd = match cmd_arg {
-            SubCommand::Restart {
-                cluster: ref cluster_name,
-            } => {
-                vec![
-                    SubCommand::Stop {
-                        cluster: cluster_name.clone(),
-                        force: false,
-                        all: false,
-                    },
-                    SubCommand::Start {
-                        cluster: cluster_name.to_string(),
-                    },
-                ]
+    ) -> Result<TaskExecutionContext> {
+        let cmd_str = cmd_arg.as_ref().to_owned();
+        let (barrier, executable) = match cmd_arg {
+            SubCommand::Restart { cluster } => {
+                let stop_cmd = SubCommand::Stop {
+                    cluster: cluster.clone(),
+                    force: false,
+                    all: false,
+                };
+                let (mut barrier, mut executable) = self.stop_tasks(stop_cmd, &config);
+                let start_cmd = SubCommand::Start { cluster };
+                let (b, exe) = self.start_tasks(start_cmd, &config);
+                barrier.extend(b);
+                executable.extend(exe);
+                (barrier, executable)
             }
-            _ => {
-                vec![cmd_arg.clone()]
+            SubCommand::Start { .. } => self.start_tasks(cmd_arg, &config),
+            SubCommand::Stop { .. } => self.stop_tasks(cmd_arg, &config),
+            SubCommand::Status { .. } => {
+                let tasks = self.status_tasks(cmd_arg, &config);
+                (vec![tasks.len()], tasks)
             }
+            _ => unreachable!(),
         };
 
-        for cmd in batch_cmd {
-            let curr_cmd_ref = cmd.as_ref();
-            let log_srv_tasks = MonographLogCtlTask::from_config(cmd.clone(), &config);
-            let log_probe_tasks = if curr_cmd_ref.eq("start") {
-                MonographLogProbeTask::from_config(&config)
-            } else {
-                IndexMap::new()
-            };
-            let tx_srv_tasks = MonographTxCtlTask::from_config(cmd.clone(), &config);
-            barrier.push(log_srv_tasks.len());
-            if !log_probe_tasks.is_empty() {
-                barrier.push(log_probe_tasks.len());
-            }
-            barrier.push(tx_srv_tasks.len());
+        Ok(TaskExecutionContext {
+            task_group: format!("cluster-control-{cmd_str}"),
+            barrier: Some(barrier),
+            executable,
+        })
+    }
+}
 
-            executable.extend(log_srv_tasks.into_iter());
-            if !log_probe_tasks.is_empty() {
-                executable.extend(log_probe_tasks.into_iter());
+impl CtrlDBTaskGroup {
+    fn stop_tasks(
+        &self,
+        stop_cmd: SubCommand,
+        config: &DeployConfig,
+    ) -> (Vec<usize>, IndexMap<TaskId, TaskInstance>) {
+        let stop_cass = match stop_cmd {
+            SubCommand::Stop { all, .. } => all,
+            _ => unreachable!(),
+        };
+        let deployment = &config.deployment;
+        let mut barrier = vec![];
+        let mut executable = IndexMap::new();
+
+        if deployment.codis.is_some() {
+            let codis_tasks = CodisTask::from_config(&config, codis_task::Operation::Stop);
+            if !codis_tasks.is_empty() {
+                barrier.push(codis_tasks.len());
+                executable.extend(codis_tasks);
             }
-            executable.extend(tx_srv_tasks.into_iter());
         }
 
-        if config.deployment.product() == deployment::Product::EloqKV
-            && config.deployment.codis.is_some()
-        {
-            let codis_tasks = if cmd_ref == "start" {
-                CodisTask::from_config(&config, codis_task::Operation::Start)
-            } else if cmd_ref == "stop" {
-                CodisTask::from_config(&config, codis_task::Operation::Stop)
-            } else {
-                IndexMap::default()
-            };
+        // stop order: tx-server -> log-server -> cassandra
+        let stop_tx = MonographTxCtlTask::from_config(stop_cmd.clone(), &config);
+        barrier.push(stop_tx.len());
+        executable.extend(stop_tx);
+        if deployment.log_service.is_some() {
+            let stop_log = MonographLogCtlTask::from_config(stop_cmd.clone(), &config);
+            barrier.push(stop_log.len());
+            executable.extend(stop_log);
+        }
+        if stop_cass && deployment.storage_service.inner_cass().is_some() {
+            let tasks = CassandraCtlTask::from_config(stop_cmd, &config);
+            barrier.push(tasks.len());
+            executable.extend(tasks);
+        }
+        (barrier, executable)
+    }
+
+    fn start_tasks(
+        &self,
+        start_cmd: SubCommand,
+        config: &DeployConfig,
+    ) -> (Vec<usize>, IndexMap<TaskId, TaskInstance>) {
+        let deployment = &config.deployment;
+        let mut barrier = vec![];
+        let mut executable = IndexMap::new();
+        // start order: cassandra -> log-server -> tx-server
+        if deployment.storage_service.inner_cass().is_some() {
+            let tasks = CassandraCtlTask::from_config(start_cmd.clone(), &config);
+            let ba = CassandraCtlTask::start_barrier(tasks.len());
+            barrier.extend(ba);
+            executable.extend(tasks);
+        }
+        if deployment.log_service.is_some() {
+            let start_log = MonographLogCtlTask::from_config(start_cmd.clone(), &config);
+            barrier.push(start_log.len());
+            executable.extend(start_log);
+            let probe = MonographLogProbeTask::from_config(&config);
+            barrier.push(probe.len());
+            executable.extend(probe);
+        }
+        let start_tx = MonographTxCtlTask::from_config(start_cmd, &config);
+        barrier.push(start_tx.len());
+        executable.extend(start_tx);
+
+        if deployment.codis.is_some() {
+            let codis_tasks = CodisTask::from_config(&config, codis_task::Operation::Start);
             if !codis_tasks.is_empty() {
                 // start dashboard firstly, and then start all proxy servers
                 barrier.push(1);
@@ -97,11 +125,25 @@ impl TaskGroup for CtrlDBTaskGroup {
             }
         }
 
-        let final_barrier = if is_start_cmd { Some(barrier) } else { None };
-        Ok(TaskExecutionContext {
-            task_group: format!("cluster-control-{cmd_ref}"),
-            barrier: final_barrier,
-            executable,
-        })
+        (barrier, executable)
+    }
+
+    fn status_tasks(
+        &self,
+        cmd: SubCommand,
+        config: &DeployConfig,
+    ) -> IndexMap<TaskId, TaskInstance> {
+        let deployment = &config.deployment;
+        let mut executable = IndexMap::new();
+        if deployment.log_service.is_some() {
+            let tasks = MonographLogCtlTask::from_config(cmd.clone(), &config);
+            executable.extend(tasks);
+        }
+        let start_tx = MonographTxCtlTask::from_config(cmd, &config);
+        executable.extend(start_tx);
+        if deployment.codis.is_some() {
+            //TODO
+        }
+        executable
     }
 }
