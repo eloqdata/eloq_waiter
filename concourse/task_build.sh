cd14e4c451a55434f35f5bd155c2b55924768c5a
JoJo <zx910819zx@163.com>
2024-10-09 15:03:32 +0800

    update ci pipeline

--- concourse/task_build.sh
+++ concourse/task_build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -exo pipefail
 
-# Install cargo-make
 cargo install cargo-make
-
-# Build rest_api_pkg
 cd monograph_waiter
-cargo make --no-workspace --makefile Makefile.toml rest_api_pkg
-tar -czvf eloqctl.tar.gz eloqctl
 
 # Determine OS version
 source /etc/os-release
@@ -26,21 +21,21 @@ esac
 
 # Handle tagged versions
 if [ -n "${TAGGED}" ]; then
-    TAGGED=$(git tag --points-at HEAD --sort=-version:refname | head -n 1)
-    if [ -z "${TAGGED}" ]; then
+    TAG=$(git tag --points-at HEAD --sort=-version:refname | head -n 1)
+    if [ -z "${TAG}" ]; then
         echo "No tag found for HEAD. Exiting."
         exit 1
     fi
-    scripts/git-checkout.sh "${TAGGED}"
+else
+    TAG="main"
 fi
 
-# Set tarball name
-OUT_NAME=${OUT_NAME:-"default"}
-if [ -n "${TAGGED}" ]; then
-    TX_TARBALL="eloqctl-${TAGGED}-${OS_ID}-${ARCH}.tar.gz"
-else
-    TX_TARBALL="eloqctl-${OUT_NAME}-${OS_ID}-${ARCH}.tar.gz"
-fi
+git checkout "${TAG}"
+TX_TARBALL="eloqctl-${TAG}-${OS_ID}-${ARCH}.tar.gz"
+
+# Build
+cargo make --no-workspace --makefile Makefile.toml rest_api_pkg
+tar -czvf eloqctl.tar.gz eloqctl
 
 # Upload to S3
 aws s3 cp eloqctl.tar.gz s3://eloq-release/eloqctl/${TX_TARBALL}
