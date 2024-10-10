b566a6125f4c81b3a9e9cb8470bf626bb86a47d5
JoJo <52664405+JoJossd@users.noreply.github.com>
2024-10-10 18:14:07 +0800

    add build and test pipeline for tagged version (#197)

--- concourse/task_test.sh
+++ concourse/task_test.sh
@@ -4,26 +4,28 @@ set -exo pipefail
 # prepare environment
 source /etc/os-release
 if [ "$ID" == "centos" ] || [ "$ID" == "rocky" ] || [ "$ID" == "rhel" ]; then
-  sudo /usr/sbin/sshd
+    sudo /usr/sbin/sshd
 elif [ "$ID" == "ubuntu" ]; then
-  sudo service ssh start
+    sudo service ssh start
 fi
+
 export ELOQCTL_HOME="${HOME}/.eloqctl"
-bash waiter_src/concourse/install.sh
 export PATH="$PATH:$ELOQCTL_HOME/bin"
+
+bash waiter_src/concourse/install.sh "main"
 
 cd $ELOQCTL_HOME
 cat version
 bash tests/launch.sh
-bash tests/launch_with_hot_standby.sh
-bash tests/launch_with_hot_standby_and_voter.sh
 bash tests/demo.sh
 bash tests/update.sh
 bash tests/control.sh
+bash tests/launch_with_hot_standby.sh
+bash tests/launch_with_hot_standby_and_voter.sh
 
 if [[ ! "$(python3 --version)" =~ "Python 3.12" ]]; then
-  wget https://downloads.datastax.com/enterprise/cqlsh-astra.tar.gz
-  tar -xzvf cqlsh-astra.tar.gz
-  export PATH=$PATH:${PWD}/cqlsh-astra/bin
-  bash tests/external_cass.sh 172.31.5.203
+    wget https://downloads.datastax.com/enterprise/cqlsh-astra.tar.gz
+    tar -xzvf cqlsh-astra.tar.gz
+    export PATH=$PATH:${PWD}/cqlsh-astra/bin
+    bash tests/external_cass.sh 172.31.5.203
 fi
